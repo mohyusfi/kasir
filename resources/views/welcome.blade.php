3fe6e4ed8c74a594bc4751a970d0a7901eecbb95
gitaadjah <gitaayu980@gmail.com>
2025-04-10 11:34:14 +0800

    responsive welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,24 +22,17 @@
     </head>
     <body class="bg-neutral-200 text-[#1b1b18] flex min-h-screen lg:justify-center">
 
-        <section class="flex max-w-[335px] w-full flex-col-reverse lg:max-w-4xl lg:flex-row">
-        <div class="ml-20 my-12">
-            <h1 class="mt-12 mb-1 text-8xl font-bold text-cyan-800">Welcome!</h1>
-            <h2 class="ml-2 font-bold text-4xl text-teal-500">HF Karang Rejo</h2>
-        <div class="mt-5 border-4 border-indigo-400 w-[520px] bg-indigo-50 px-5" style="border-radius:30px; font-family:arial;">
+        <section class="flex w-full flex-col-reverse lg:max-w-4xl lg:flex-row">
+        <div class="ml-12 my-12">
+            <h1 class="text-6xl -mt-5 lg:mt-12 mb-1 lg:text-8xl font-bold text-cyan-800">Welcome!</h1>
+            <h2 class="ml-2 font-bold text-2xl lg:text-4xl text-teal-500">HF Karang Rejo</h2>
+        <div class="mt-5 border-4 border-indigo-400 w-[300px] lg:w-[520px] bg-indigo-50 px-5" style="border-radius:30px; font-family:arial;">
             <h3 class="mt-8 ml-2 text-2xl">Please login to access this page</h3>
             <h4 class="mt-1 ml-2">Don't have an account? Please register to create an account</h4>
-            <div class="w-full text-sm mb-6 not-has-[nav]:hidden justify-center">
-            @if (Route::has('login'))
-                <div class="mt-8 flex items-center ml-[150px] gap-4">
+            <div class="text-sm mb-6 not-has-[nav]:hidden justify-center">
+            <div class="mt-3 flex items-center gap-4">
                     @auth
-                        <a
-                            href="{{ url('/dashboard') }}"
-                            class="inline-block px-5 py-1.5 light:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] rounded-sm text-sm leading-normal "
-                        >
-                            Dashboard
-                        </a>
-                    @else
+                        @if (Route::has('login'))
                         <a
                             href="{{ route('login') }}"
                             class=" bg-cyan-700 inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18]  rounded-full text-sm leading-normal hover:bg-sky-400"
@@ -60,8 +53,9 @@
         </div>
         </div>
         </div>
+        
         <div class="flex justify-center items-center">
-    <x-application-logo class="block h-[400px] w-auto fill-current text-gray-800" />
+    <x-application-logo class="block h-[300px] w-auto fill-current text-gray-800" />
 </div>
         </section>
         
